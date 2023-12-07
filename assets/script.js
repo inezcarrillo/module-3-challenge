ca04ee46cb622a57bf76b77af4ff24000e13fbb2
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2023-12-06 18:18:13 -0600

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var passwordLength = [passwordLength<8 || passwordLength<128-characters];
-var characterTypes = ['upperCase' , 'lowerCase' , 'number' , 'specialCharacter']
+var number = ['1' , '2' , '3' , '4' , '5' , '6' ,] 
 
 //Functiion to generate password
 function generatePassword(){
-//promote user to create a password 
-    var userChoice = prompt("Please create a new password!");
 
-//Check to see if password meets lenght requirements
-if (passwordLength<8 || passwordLength>128-characters(userChoice)){
-    alert("Password meets length requirements");
-}else if (userChoice (passwordLength<8 ||passwordLength>128));
-    alert("Please select a new password!");
-    return;
-}
+    //Prompt user to select a desired password length
+    var passwordLength = prompt("How many characters do you want in you password?");
+    //Check if passwod in between 8 and 128 characters long 
+    var passwordLength = (passwordLength > 8 || passwordLength < 128)     
+    //Display alert and loop function until password length is met
+    alert("Password must be between 8 and 128 characters long.") ;
+    return; 
+    }if (passwordLength > 8 || passwordLength < 128);
+        console.log("Please select a number");
+
+
 
 // Write password to the #password input
 function writePassword() {
